fe4d4f46801ea918482ace1bd7d62c5d24b86f09
Mohammed Usama <usamabhakrani@gmail.com>
2023-08-12 15:35:02 +0500

    Refactor Import/Export and folder structure

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,58 +1,19 @@
-import { configureStore, createSlice, createAction } from "@reduxjs/toolkit";
-
-export const reset = createAction("app/reset");
-console.log(reset());
-
-const songsSlice = createSlice({
-  name: "song",
-  initialState: [],
-  reducers: {
-    addSong(state, action) {
-      state.push(action.payload);
-    },
-    removeSong(state, action) {
-      const index = state.indexOf(action.payload);
-      state.splice(index, 1);
-    },
-  },
-  extraReducers(builder) {
-    // builder.addCase(moviesSlice.actions.reset, (state, action) => []);
-    builder.addCase(reset, (state, action) => []);
-  },
-});
-
-const moviesSlice = createSlice({
-  name: "movie",
-  initialState: [],
-  reducers: {
-    addMovie(state, action) {
-      state.push(action.payload);
-    },
-    removeMovie(state, action) {
-      const index = state.indexOf(action.payload);
-      state.splice(index, 1);
-    },
-
-    // reset(state, action) {
-    //   return [];
-    // },
-  },
-  extraReducers(builder) {
-    builder.addCase(reset, (state, action) => []);
-  },
-});
+import { configureStore } from "@reduxjs/toolkit";
+import { songsReducer, addSong, removeSong } from "./slices/songsSlice";
+import { moviesReducer, addMovie, removeMovie } from "./slices/moviesSlice";
+import { reset } from "./actions";
 
 const store = configureStore({
   reducer: {
-    songs: songsSlice.reducer,
-    movies: moviesSlice.reducer,
+    songs: songsReducer,
+    movies: moviesReducer,
   },
 });
 
 // const startingState = store.getState();
 // console.log(startingState);
 
-store.dispatch(songsSlice.actions.addSong("latest song"));
+// store.dispatch(songsSlice.actions.addSong("latest song"));
 
 // store.dispatch({
 //   type: "song/addSong",
@@ -62,6 +23,4 @@ store.dispatch(songsSlice.actions.addSong("latest song"));
 // const finalState = store.getState();
 // console.log(finalState);
 
-export { store };
-export const { addSong, removeSong } = songsSlice.actions;
-export const { addMovie, removeMovie } = moviesSlice.actions;
+export { store, reset, addSong, removeSong, addMovie, removeMovie };
